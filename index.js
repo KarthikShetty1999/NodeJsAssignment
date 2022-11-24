4e199f1b08967bded39b53277a405458472e1cc1
Karthik170 <karthikkumar.shetty@niveussolution.com>
2022-11-24 11:24:28 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const promise = new Promise((resolve, reject) => {  
+        resolve('Promise is resolved successfully.');  
+        reject('Promise is rejected');  
+});
+promise.then(
+    (result) => { 
+       console.log(result);
+    },
+    (error) => { 
+       console.log(error);
+    }
+);
+
+
+
